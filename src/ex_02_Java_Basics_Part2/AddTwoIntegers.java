b510b4e5beb5d6b614eea86dea4e6f2fa40cd3c6
MySecondAttemptToFetchDataFromRemoteToLocal <surajsahu2807@gmail.com>
2025-05-11 23:28:26 +0530

    Added AddTwoIntegers file

---
+++ src/ex_02_Java_Basics_Part2/AddTwoIntegers.java
@@ -0,0 +1,9 @@
+package ex_02_Java_Basics_Part2;
+
+public class AddTwoIntegers {
+    public static void main(String[] args) {
+        int n = 2+2;
+        System.out.println("Total Sum : " + n);
+    }
+
+}
